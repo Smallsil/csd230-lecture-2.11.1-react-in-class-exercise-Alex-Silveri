f7b139b10a961440df0239f8b09a54b958a045ca
Smallsil <alexsilveri17@gmail.com>
2026-04-02 19:28:28 -0400

    Workflow Run test fixed 2.0

--- src/test/java/csd230/ApplicationTests.java
+++ src/test/java/csd230/ApplicationTests.java
@@ -1,13 +1,14 @@
 package csd230;
 
+import csd230.bookstore.Application;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Application.class)
 class ApplicationTests {
 
     @Test
     void contextLoads() {
+        // This test will pass if the application context loads successfully
     }
-
 }
